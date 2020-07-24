66884a4b05e99bdc42e152ea44663c70154e6b79
sathish kumar <sathishds2428@gmail.com>
2020-07-24 11:53:40 +0530

    Updates

---
+++ precache-manifest.d3a322cc19d6267ec1fadcf0bed12240.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d337afa44faa13eb425de5b9d5c33f75",
+    "url": "/urctweather-app/index.html"
+  },
+  {
+    "revision": "a72692740cc6bfc8b587",
+    "url": "/urctweather-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3e79645755f19f4615cb",
+    "url": "/urctweather-app/static/js/2.60572587.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/urctweather-app/static/js/2.60572587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72692740cc6bfc8b587",
+    "url": "/urctweather-app/static/js/main.a90ef72a.chunk.js"
+  },
+  {
+    "revision": "917852f9e9be72681b30",
+    "url": "/urctweather-app/static/js/runtime-main.5f79b9b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/urctweather-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
